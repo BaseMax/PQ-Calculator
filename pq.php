f41e481b1a6d5d5784e49f552cc71da7c733b55f
Max Base <MaxBaseCode@Gmail.Com>
2021-09-07 10:20:54 +0430

    Create pq.php

---
+++ pq.php
@@ -0,0 +1,11 @@
+<?php
+// https://github.com/BaseMax/PQ-Calculator
+
+$pa=getcwd();
+if(strncasecmp(PHP_OS, 'WIN', 3) == 0) {
+    echo exec($pa.'/a.exe'." ". $_GET["num"]);
+}
+else {
+    echo exec($pa.'/a.out'." ". $_GET["num"]);
+}
+?>
